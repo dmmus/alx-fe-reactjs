d2f243dedcaf744f623ce87ef9a6014ba9c3bc00
Donat MUSEME <musemeprince@gmail.com>
2026-01-11 22:33:10 +0100

    fifth commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import UserProfile from './components/UserProfile.jsx'
 import Footer from './components/Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -16,6 +17,12 @@ function App() {
         <Header />
         <MainContent />
         <WelcomeMessage />
+        {/* Utilisation de UserProfile avec des props personnalisées */}
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
         <Footer />
       </div>
       <div className="logos">  
