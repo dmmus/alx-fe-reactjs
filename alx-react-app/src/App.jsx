5d487f21db80c6e00e3b47088938ba4644008409
Donat MUSEME <musemeprince@gmail.com>
2026-01-11 22:21:57 +0100

    fourth commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Footer from './components/Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -10,7 +13,12 @@ function App() {
   return (
     <>
       <div>
-        <WelcomeMessage />  
+        <Header />
+        <MainContent />
+        <WelcomeMessage />
+        <Footer />
+      </div>
+      <div className="logos">  
 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
