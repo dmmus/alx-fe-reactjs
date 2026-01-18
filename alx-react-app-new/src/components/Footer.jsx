c98ab0d2606bafaeda9d5ebf3c7070152ad74297
Donat MUSEME <musemeprince@gmail.com>
2026-01-18 20:46:44 +0100

    1. Create a Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,7 +2,10 @@
 function Footer() {
     return (
         <footer>
-             <p style={{ color:"skyblue", fontSize: "20px", fontWeight:"bold"}}>© 2023 City Lovers</p>
+             <p style={{ 
+                color:"skyblue", 
+                fontSize: "20px", 
+                fontWeight:"bold"}}>© 2023 City Lovers</p>
         </footer>
     );
 }
