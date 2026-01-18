176d7b93b6be3a48e072f99cb00d68a5c6aeea42
Donat MUSEME <musemeprince@gmail.com>
2026-01-18 20:39:23 +0100

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return (
+        <footer>
+             <p style={{ color:"skyblue", fontSize: "20px", fontWeight:"bold"}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
