03706c688c3859d46c9f1f63b5c6aa70ef17d9a8
Donat MUSEME <musemeprince@gmail.com>
2026-01-25 01:07:44 +0100

    3. Build a Simple Company Website with React fix bug

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Services from "./components/Services.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <main>
-        <Outlet />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </main>
     </>
   )
--- my-company/src/main.jsx
+++ my-company/src/main.jsx
@@ -2,27 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Contact from "./components/Contact.jsx"
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import About from './components/About.jsx'
-import Home from "./components/Home.jsx"
-import Services from "./components/Services.jsx"
-
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App />,
-    children: [
-      {path:"/", Component:Home },
-      {path:"/contact", Component:Contact},
-      {path:"/about", Component:About},
-      {path:"/services", Component:Services},
-    ]
-  }
-]);
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
