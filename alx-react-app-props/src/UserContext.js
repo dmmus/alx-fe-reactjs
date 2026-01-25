f22f255911f0590fa177984b78858df93c312fe4
Donat MUSEME <musemeprince@gmail.com>
2026-01-25 01:30:04 +0100

    2. Refactor Prop Drilling to Use Context API bug 2

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    name: "John Doe",
+    email: "john@example.com"
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
